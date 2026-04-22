8f494aa20d82f80db08aa4f309aca87ad980bfa7
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2026-04-22 05:08:36 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,12 +15,6 @@ const nextConfig: NextConfig = {
 
   // تحسين الصور
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-      },
-    ],
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 يوم كاش
     remotePatterns: [
